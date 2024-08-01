05870f1578cd8226dc0eb84cc3edfcc490bb067a
Harsh Soni <algebra.soni@gmail.com>
2024-08-01 17:09:06 +0000

    fix: import WalletSelector and it's CSS file

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 
 function App() {
   return (
