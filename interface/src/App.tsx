8af391f28835356f505045c74df31281e375d8b2
Harsh Soni <algebra.soni@gmail.com>
2024-08-01 17:13:25 +0000

    wallet: add wallet UI

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -2,27 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import styled from "styled-components";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 
-function App() {
+const WindowWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+const WalletWrapper = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  background-color: #f0f0f0;
+`;
+
+const App: React.FC = () => {
+  const { connected } = useWallet();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WindowWrapper>
+      <WalletWrapper>
+        <WalletSelector />
+      </WalletWrapper>
+      {connected ? (
+        <h1>Wallet is connected</h1>
+      ) : (
+        <h1>Please connect your wallet to continue</h1>
+      )}
+    </WindowWrapper>
   );
-}
+};
 
 export default App;
