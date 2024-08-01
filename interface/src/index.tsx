c32eae9cd051981763852482868c153e2e7d21d3
Harsh Soni <algebra.soni@gmail.com>
2024-08-01 17:06:07 +0000

    wallet: add wallet support

--- interface/src/index.tsx
+++ interface/src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+
+const wallets = [
+  new PetraWallet()
+];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AptosWalletAdapterProvider plugins={wallets} autoConnect={false}>
+      <App />
+    </AptosWalletAdapterProvider>
   </React.StrictMode>
 );
 
